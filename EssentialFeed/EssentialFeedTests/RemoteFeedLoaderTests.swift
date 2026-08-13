ece7053bb2e8ff1e3d46c44578b1926a07bb10ae
Eyuphan <akkayaeyupp@gmail.com>
2026-08-13 11:22:50 +0300

    test init does not request data from url

---
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,30 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Eyüphan Akkaya on 13.08.2026.
+//
+
+import XCTest
+
+final class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestURL = URL(string: "https://example.com/feed")!
+    }
+}
+
+final class HTTPClient {
+    static var shared = HTTPClient()
+    
+    var requestURL: URL?
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
+        let _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestURL)
+    }
+    
+}
